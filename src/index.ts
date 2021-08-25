2662ec45eb8ffb781971795d0918ae886b17eb7c
Binzy <xbinzy@protonmail.com>
2021-08-25 13:41:14 +0100

    build: Binzy doz it

--- src/index.ts
+++ src/index.ts
@@ -2033,11 +2033,11 @@ function detectCollisions() {
         // playerDamageGiven[currentPlayer.id + pack.id] = now
         // // console.log('Player Damage Given', currentPlayer.id + pack.id)
         // if (playerDamageTaken[currentPlayer.id + pack.id] > now - 500) {
-          if (player1.xp > 5) {
-            player1.xp -= 1
-          } else {
+          // if (player1.xp > 5) {
+          //   player1.xp -= 1
+          // } else {
             registerKill(player2, player1)
-          }
+          // }
           break
         // }
       } else if (player1.avatar > player2.avatar) {
@@ -2045,11 +2045,11 @@ function detectCollisions() {
         // playerDamageGiven[pack.id + currentPlayer.id] = now
         // // console.log('Player Damage Given', pack.id + currentPlayer.id)
         // if (playerDamageTaken[pack.id + currentPlayer.id] > now - 500) {
-          if (player2.xp > 5) {
-            player2.xp -= 1
-          } else {
+          // if (player2.xp > 5) {
+          //   player2.xp -= 1
+          // } else {
             registerKill(player1, player2)
-          }
+          // }
           break
         // }
       }
